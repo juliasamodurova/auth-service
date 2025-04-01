eb1a4b1ce5cac97e0e215693ab64f5cffa7e9dea
Julia Samodurova <157882941+juliasamodurova@users.noreply.github.com>
2025-04-01 20:50:31 +0300

    Delete internal directory

--- internal/service/entity.go
+++
@@ -1,9 +0,0 @@
-package service
-
-const (
-	ErrUnknown              = "try it a little later or check the data you entered"
-	ErrUserAuthAlreadyExist = "user auth already exist"
-	ErrUserNotFound         = "User not found"
-	ErrValidateJwt          = "not authorized"
-	ErrTokenNotFound        = "refresh token not found"
-)
